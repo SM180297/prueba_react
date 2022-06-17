8146cdf5fbf221f44cb58b602a817fddcac1f279
SM180297 <edwinsantos74447@gmail.com>
2022-06-17 10:49:37 -1200

    primera pagina

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         hola mundo este es mi primer trabajo 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://werrstation.tk/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Werrstation
         </a>
       </header>
     </div>
